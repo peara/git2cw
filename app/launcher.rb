9dcef9ccb4628d96b3eafd1fed286b3604cf133a
Vu Ngoc Quang <vu.ngoc.quangb@framgia.vn>
2017-11-23 15:26:03 +0700

    Check for repo events

--- app/launcher.rb
+++ app/launcher.rb
@@ -4,11 +4,12 @@ require 'dotenv/load'
 require 'octokit'
 require 'chatwork'
 require_relative 'message_generator'
+require 'pry'
 
 class Launcher
   attr_accessor :git_client, :chatwork_client
   def initialize
-    puts "Launching"
+    puts "Launching..."
 
     puts "Load Github Configs"
     @git_client = Octokit::Client.new(:access_token => ENV["GITHUB_API_KEY"])
@@ -19,25 +20,67 @@ class Launcher
     puts "Load Chatwork"
     ChatWork.api_key = ENV["CHATWORK_API_KEY"]
 
-    puts "Loading... done!\n"
+    puts "Load Stored File"
+    if File.file?('data.txt')
+      File.open('data.txt', 'r') do |file|
+        @last_event_id = file.gets.to_i
+      end
+    else
+      @last_event_id = 0
+    end
+
+    puts "Loading... done!\n\n"
+  end
+
+  # TODO: not yet complete
+  def check_new_noti
+    Settings.Repos.each do |repo|
+      notis = @git_client.repository_events(repo.url)
+      next if notis.empty?
+
+      puts "New Notification in #{repo.display_name}"
+
+      notis.each do |noti|
+        event = launcher.git_client.get(noti.subject.latest_comment_url)
+        next unless noti.subject.type == "PullRequest"
+        message = MessageGenerator.noti2message repo, noti, event
+        ChatWork::Message.create(room_id: repo.chatwork_box, body: message)
+      end
+
+      launcher.git_client.mark_repo_notifications_as_read(repo.url) if repo.auto_read
+    end
+  end
+
+  def check_new_event
+    @max_event_id = @last_event_id
+    puts "Checking new events"
+    Settings.Repos.each do |repo|
+      puts "\tRepo: #{repo.display_name}"
+      events = @git_client.repository_events(repo.url)
+
+      events.each do |event|
+        break if event.id.to_i <= @last_event_id
+        message = MessageGenerator.event2message event
+        ChatWork::Message.create(room_id: repo.chatwork_box, body: message) if message.present?
+
+        if @last_event_id == 0
+          @last_event_id = event.id.to_i
+        end
+      end
+
+      @max_event_id = events[0].id.to_i if events[0].id.to_i > @max_event_id
+    end
+  end
+
+  def shutdown
+    puts "Shutting down..."
+    File.open('data.txt', 'w') do |file|
+      file.puts @max_event_id
+    end
+    puts "Done!"
   end
 end
 
 launcher = Launcher.new
-Settings.Repos.each do |repo|
-  notis = launcher.git_client.repository_notifications(repo.url)
-  next if notis.empty?
-
-  puts "New Notification in #{repo.display_name}"
-
-  notis.each do |noti|
-    event = launcher.git_client.get(noti.subject.latest_comment_url)
-    next unless noti.subject.type == "PullRequest"
-    message = MessageGenerator.gen repo, noti, event
-    ChatWork::Message.create(room_id: repo.chatwork_box, body: message)
-  end
-
-  launcher.git_client.mark_repo_notifications_as_read(repo.url) if repo.auto_read
-end
-
-puts "Finished!"
+launcher.check_new_event
+launcher.shutdown
--- app/message_generator.rb
+++ app/message_generator.rb
@@ -1,5 +1,5 @@
 class MessageGenerator
-  def self.gen repo, noti, event
+  def self.noti2message repo, noti, event
     description = event.body
 
     message = <<~MSG
@@ -8,6 +8,43 @@ class MessageGenerator
       Url: #{event.html_url}
       User: #{event.user.login}
       Comment: #{description}
+      [/info]
+    MSG
+
+    message
+  end
+
+  def self.event2message event
+    repo = event.repo
+    title = ''
+    url = ''
+    user = event.actor.login
+    body = ''
+
+    case event.type
+    when 'PullRequestEvent'
+      title = "#{event.actor.login} #{event.payload.action} pull request #{event.payload.pull_request.number}"
+      url = event.payload.pull_request.html_url
+      body = event.payload.pull_request.title
+    when 'IssuesEvent'
+      title = "#{event.actor.login} #{event.payload.action} issue #{event.payload.issue.number}"
+      url = event.payload.issue.html_url
+      body = event.payload.issue.title
+    when 'IssueCommentEvent'
+      title = "#{event.actor.login} #{event.payload.action} comment in issue #{event.payload.issue.number}"
+      url = event.payload.comment.html_url
+      body = "[code]\n#{event.payload.comment.body}\n[/code]"
+    else
+      return nil
+    end
+
+
+    message = <<~MSG
+      [info][title][Github] #{repo.name} [/title]
+      Title: #{title}
+      Url: #{url}
+      User: #{user}
+      Body: #{body}
       [/info]
     MSG
 
